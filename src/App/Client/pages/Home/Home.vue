870960c86b81d1abd9c7ff050af52f70da6f6973
Teixeira <nicolasteixeira3856@outlook.com>
2019-10-30 16:35:34 -0300

    Adicionando o componente de cadastro, reformulando o componente de home, colocando cards no local do carousel

--- src/App/Client/pages/Home/Home.vue
+++ src/App/Client/pages/Home/Home.vue
@@ -1,16 +1,46 @@
 <template>
     <div>
-        <Carousel></Carousel>
+        <b-row>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+            <b-col lg="4">
+                <Card></Card>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col lg="12">
+                <Cadastro></Cadastro>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
-    import Carousel from '../../components/Carousel/Carousel'
+    import Carousel from '../../components/Carousel/Carousel';
+    import Card from "../../components/Card/Card";
+    import Cadastro from "../../components/Cadastro/Cadastro";
 
     export default {
         name: "Home",
         components: {
-            Carousel
+            Carousel,
+            Card,
+            Cadastro
         }
     }
 </script>
